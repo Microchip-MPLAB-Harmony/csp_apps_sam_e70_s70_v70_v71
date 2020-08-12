7df5484085ce1f4c9f6ebe30d68e09fb33544bbf
Amit Raddi <amit.raddi@microchip.com>
2020-08-12 15:00:26 +0530

    [AFEC] Regenerated demo for afec_afec_user_sequence_sam_v71_xult
    MH3-43203
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/afec/afec_user_sequence/firmware/src/config/sam_v71_xult/peripheral/tc/plib_tc1.h
+++ apps/afec/afec_user_sequence/firmware/src/config/sam_v71_xult/peripheral/tc/plib_tc1.h
@@ -120,8 +120,10 @@ TC_COMPARE_STATUS TC1_CH0_CompareStatusGet(void);
  
 
 
+#ifdef __cplusplus // Provide C++ Compatibility
+}
+#endif
+
 #endif //PLIB_TC1_H
 
-/**
- End of File
-*/
+/* End of File */
