7576f13cfc137403731394f70dabc9877ec7c351
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-02-01 09:51:19 +0530

    [MCAN] Regenerated demo for mcan_mcan_normal_operation_blocking_sam_e70_xult
    MH3-49272
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/mcan/mcan_normal_operation_blocking/firmware/src/config/sam_e70_xult/peripheral/mcan/plib_mcan1.h
+++ apps/mcan/mcan_normal_operation_blocking/firmware/src/config/sam_e70_xult/peripheral/mcan/plib_mcan1.h
@@ -105,6 +105,8 @@ bool MCAN1_TxFIFOIsFull(void);
 void MCAN1_MessageRAMConfigSet(uint8_t *msgRAMConfigBaseAddress);
 bool MCAN1_StandardFilterElementSet(uint8_t filterNumber, mcan_sidfe_registers_t *stdMsgIDFilterElement);
 bool MCAN1_StandardFilterElementGet(uint8_t filterNumber, mcan_sidfe_registers_t *stdMsgIDFilterElement);
+void MCAN1_SleepModeEnter(void);
+void MCAN1_SleepModeExit(void);
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
     }
