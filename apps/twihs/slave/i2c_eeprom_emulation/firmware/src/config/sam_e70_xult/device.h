7d1053ed07cf96b152eb2e6eb0ddaedd25e7419a
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-02-01 11:23:52 +0530

    [TWIHS] Regenerated demo for twihs_slave_i2c_eeprom_emulation_sam_e70_xult
    MH3-49333
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/twihs/slave/i2c_eeprom_emulation/firmware/src/config/sam_e70_xult/device.h
+++ apps/twihs/slave/i2c_eeprom_emulation/firmware/src/config/sam_e70_xult/device.h
@@ -57,7 +57,7 @@
 #ifndef DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS
     #define DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS
 #endif //DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS
-#include "atsame70q21b.h"
+#include "same70q21b.h"
 #pragma GCC diagnostic pop
 #include "device_cache.h"
 #include "toolchain_specifics.h"
