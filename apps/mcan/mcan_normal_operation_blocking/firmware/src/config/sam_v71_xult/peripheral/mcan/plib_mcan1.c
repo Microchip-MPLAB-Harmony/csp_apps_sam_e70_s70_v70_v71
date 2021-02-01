3472034fad6a1583ccb53f8b7ee37dab017e59c2
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-02-01 09:53:03 +0530

    [MCAN] Regenerated demo for mcan_mcan_normal_operation_blocking_sam_v71_xult
    MH3-49273
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/mcan/mcan_normal_operation_blocking/firmware/src/config/sam_v71_xult/peripheral/mcan/plib_mcan1.c
+++ apps/mcan/mcan_normal_operation_blocking/firmware/src/config/sam_v71_xult/peripheral/mcan/plib_mcan1.c
@@ -51,6 +51,7 @@
 
 #include "device.h"
 #include "plib_mcan1.h"
+#include "interrupts.h"
 
 // *****************************************************************************
 // *****************************************************************************
@@ -195,7 +196,7 @@ bool MCAN1_MessageTransmit(uint32_t id, uint8_t length, uint8_t* data, MCAN_MODE
         fifo->MCAN_TXBE_0 |= MCAN_TXBE_0_RTR_Msk;
     }
 
-    fifo->MCAN_TXBE_1 |= ((++messageMarker << MCAN_TXBE_1_MM_Pos) & MCAN_TXBE_1_MM_Msk);
+    fifo->MCAN_TXBE_1 |= ((++messageMarker << MCAN_TXBE_1_MM_Pos) & MCAN_TXBE_1_MM_Msk) | MCAN_TXBE_1_EFC_Msk;
 
     /* request the transmit */
     MCAN1_REGS->MCAN_TXBAR = 1U << tfqpi;
@@ -644,6 +645,29 @@ bool MCAN1_StandardFilterElementGet(uint8_t filterNumber, mcan_sidfe_registers_t
     return true;
 }
 
+
+void MCAN1_SleepModeEnter(void)
+{
+    MCAN1_REGS->MCAN_CCCR |=  MCAN_CCCR_CSR_Msk;
+    while ((MCAN1_REGS->MCAN_CCCR & MCAN_CCCR_CSA_Msk) != MCAN_CCCR_CSA_Msk)
+    {
+        /* Wait for clock stop request to complete */
+    }
+}
+
+void MCAN1_SleepModeExit(void)
+{
+    MCAN1_REGS->MCAN_CCCR &=  ~MCAN_CCCR_CSR_Msk;
+    while ((MCAN1_REGS->MCAN_CCCR & MCAN_CCCR_CSA_Msk) == MCAN_CCCR_CSA_Msk)
+    {
+        /* Wait for no clock stop */
+    }
+    MCAN1_REGS->MCAN_CCCR &= ~MCAN_CCCR_INIT_Msk;
+    while ((MCAN1_REGS->MCAN_CCCR & MCAN_CCCR_INIT_Msk) == MCAN_CCCR_INIT_Msk)
+    {
+        /* Wait for initialization complete */
+    }
+}
 
 
 
