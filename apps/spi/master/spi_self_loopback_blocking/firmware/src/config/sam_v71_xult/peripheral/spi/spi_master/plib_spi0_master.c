80e4ab4dc866b36871232eb00621d58dc273e2c5
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-08-02 19:16:24 +0530

    [SPI] Regenerated demo for spi_master_spi_self_loopback_blocking_sam_v71_xult
    MH3-58668
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/spi/master/spi_self_loopback_blocking/firmware/src/config/sam_v71_xult/peripheral/spi/spi_master/plib_spi0_master.c
+++ apps/spi/master/spi_self_loopback_blocking/firmware/src/config/sam_v71_xult/peripheral/spi/spi_master/plib_spi0_master.c
@@ -215,3 +215,8 @@ bool SPI0_TransferSetup( SPI_TRANSFER_SETUP * setup, uint32_t spiSourceClock )
     return true;
 }
 
+bool SPI0_IsTransmitterBusy( void )
+{
+    return ((SPI0_REGS->SPI_SR & SPI_SR_TXEMPTY_Msk) == 0)? true : false;
+}
+
