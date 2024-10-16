d6249404395a16f1fde7e03253008cfa10e368d2
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-16 18:23:58 +0530

    [SUPC] Regenerated demo for supc__supc_wakeup_wkup__firmware__sam_e70_xult.X
    MH3-101976

--- apps/supc/supc_wakeup_wkup/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/supc/supc_wakeup_wkup/firmware/src/config/sam_e70_xult/definitions.h
@@ -66,13 +66,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAME70Q21B"
-#define DEVICE_ARCH			 "CORTEX-M7"
-#define DEVICE_FAMILY		 "SAME"
-#define DEVICE_SERIES		 "SAME70"
+#define DEVICE_NAME          "ATSAME70Q21B"
+#define DEVICE_ARCH          "CORTEX-M7"
+#define DEVICE_FAMILY        "SAME"
+#define DEVICE_SERIES        "SAME70"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 300000000
+#define CPU_CLOCK_FREQUENCY 300000000U
 
 // *****************************************************************************
 // *****************************************************************************
