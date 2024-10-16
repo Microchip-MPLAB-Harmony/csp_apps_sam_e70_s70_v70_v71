7429569c1c3ac4dc7a685b9df74e2d0fefbbf8ac
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-16 18:31:56 +0530

    [SPI] Regenerated demo for spi__master__spi_ping_pong_with_dma__firmware__sam_v71_xult.X
    MH3-101954

--- apps/spi/master/spi_ping_pong_with_dma/firmware/src/config/sam_v71_xult/peripheral/xdmac/plib_xdmac_common.h
+++ apps/spi/master/spi_ping_pong_with_dma/firmware/src/config/sam_v71_xult/peripheral/xdmac/plib_xdmac_common.h
@@ -55,6 +55,7 @@
 */
 
 #include <stddef.h>
+#include "toolchain_specifics.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -133,7 +134,7 @@ typedef uint32_t XDMAC_CHANNEL_CONFIG;
     device data sheet to determine availability.
 */
 
-/* MISRA C-2012 Rule 6.1 deviated:5 Deviation record ID -  H3_MISRAC_2012_R_6_1_DR_1 */
+/* MISRA C-2012 Rule 6.1 deviated 6 times. Deviation record ID -  H3_MISRAC_2012_R_6_1_DR_1 */
 
 typedef union
 {
@@ -206,7 +207,8 @@ typedef struct {
 */
 
 /* View 0 */
-typedef struct {
+typedef struct
+{
 
     /* Next Descriptor Address number. */
     uint32_t mbr_nda;
@@ -217,10 +219,12 @@ typedef struct {
     /* Destination Address Member. */
     uint32_t mbr_da;
 
+
 } XDMAC_DESCRIPTOR_VIEW_0;
 
 /* View 1 */
-typedef struct {
+typedef struct
+{
 
     /* Next Descriptor Address number. */
     uint32_t mbr_nda;
@@ -234,10 +238,12 @@ typedef struct {
     /* Destination Address Member. */
     uint32_t mbr_da;
 
+
 } XDMAC_DESCRIPTOR_VIEW_1;
 
 /* View 2 */
-typedef struct {
+typedef struct
+{
 
     /* Next Descriptor Address number. */
     uint32_t mbr_nda;
@@ -255,10 +261,12 @@ typedef struct {
     /* TODO: Redefine type to XDMAC_CC white updating to N type */
     uint32_t mbr_cfg;
 
+
 } XDMAC_DESCRIPTOR_VIEW_2;
 
 /* View 3 */
-typedef struct {
+typedef struct
+{
 
     /* Next Descriptor Address number. */
     uint32_t mbr_nda;
@@ -286,6 +294,7 @@ typedef struct {
 
     /* Destination Micro-block Stride Member. */
     uint32_t mbr_dus;
+
 
 } XDMAC_DESCRIPTOR_VIEW_3;
 
