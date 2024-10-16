4a619f0d17a93d2d6d5575c7009b958144881a07
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-16 18:23:52 +0530

    [RTT] Regenerated demo for rtt__rtt_periodic_timeout__firmware__sam_e70_xult.X
    MH3-101944

--- apps/rtt/rtt_periodic_timeout/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/rtt/rtt_periodic_timeout/firmware/src/config/sam_e70_xult/definitions.h
@@ -51,8 +51,8 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/efc/plib_efc.h"
 #include "peripheral/rtt/plib_rtt.h"
+#include "peripheral/efc/plib_efc.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -63,13 +63,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAME70Q21B"
-#define DEVICE_ARCH			 "CORTEX-M7"
-#define DEVICE_FAMILY		 "SAME"
-#define DEVICE_SERIES		 "SAME70"
+#define DEVICE_NAME          "ATSAME70Q21B"
+#define DEVICE_ARCH          "CORTEX-M7"
+#define DEVICE_FAMILY        "SAME"
+#define DEVICE_SERIES        "SAME70"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 300000000
+#define CPU_CLOCK_FREQUENCY 300000000U
 
 // *****************************************************************************
 // *****************************************************************************
