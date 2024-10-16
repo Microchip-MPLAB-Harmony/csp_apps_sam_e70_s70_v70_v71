e2278f7b8225d5d668fbfd30fce31dbc1ed3b3cb
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-16 18:12:22 +0530

    [QSPI] Regenerated demo for qspi__qspi_xip_function__firmware__sam_e70_xult.X
    MH3-101928

--- apps/qspi/qspi_xip_function/firmware/src/config/sam_e70_xult/initialization.c
+++ apps/qspi/qspi_xip_function/firmware/src/config/sam_e70_xult/initialization.c
@@ -47,7 +47,6 @@
 #include "device.h"
 
 
-
 // ****************************************************************************
 // ****************************************************************************
 // Section: Configuration Bits
@@ -65,6 +64,12 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
+/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
 
 
 // *****************************************************************************
@@ -113,10 +118,11 @@ static void STDIO_BufferModeSet(void)
 
     /* Make stdout unbuffered */
     setbuf(stdout, NULL);
+    /* MISRAC 2012 deviation block end */
 }
 
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -130,6 +136,7 @@ static void STDIO_BufferModeSet(void)
 
 void SYS_Initialize ( void* data )
 {
+
     /* MISRAC 2012 deviation block start */
     /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
@@ -154,14 +161,11 @@ void SYS_Initialize ( void* data )
     USART1_Initialize();
 
 
-
-
-
     NVIC_Initialize();
+
 
     /* MISRAC 2012 deviation block end */
 }
-
 
 /*******************************************************************************
  End of File
